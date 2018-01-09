08a2073738d1ab2bb93b136c06aa13983af3bc2b
Bamby <jlandyr@hotmail.com>
2018-01-09 22:39:56 +0100

    example of kotlin language, saveIntanceState and Log

--- app/src/main/java/com/example/bamby/guedr/MainActivity.kt
+++ app/src/main/java/com/example/bamby/guedr/MainActivity.kt
@@ -6,9 +6,28 @@ import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = MainActivity::class.java.canonicalName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.v("fas", "äsdf")
+        Log.v(TAG, "onCreate")
+        if (savedInstanceState != null){
+            //Log.v(TAG, savedInstanceState.getString("clave"))
+            Log.v(TAG, "savedInstanceState !=null y la clave es: ${savedInstanceState.getString("clave")}")
+        }else{
+            Log.v(TAG, "savedInstanceState == null")
+        }
+
     }
+
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Log.v(TAG, "onSaveInstanceState")
+//        if (outState != null){
+//            outState.putString("clave","valor")
+//        }
+        outState?.putString("clave", "valor")
+   }
 }

acd3512a4f58b9eafdd0a8e9a90456972519bbba
Bamby <jlandyr@hotmail.com>
2018-01-09 22:02:56 +0100

    First commit

---
+++ app/src/main/java/com/example/bamby/guedr/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bamby.guedr
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.v("fas", "äsdf")
+    }
+}

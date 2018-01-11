aa55325b375b12803a0b57c6aa44960b6de77f78
Bamby <jlandyr@hotmail.com>
2018-01-11 01:07:28 +0100

    shows our first model on the layout

--- app/src/main/java/com/example/bamby/guedr/MainActivity.kt
+++ app/src/main/java/com/example/bamby/guedr/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.bamby.guedr
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     val TAG = MainActivity::class.java.canonicalName
     var stone_button : Button? = null
@@ -19,80 +18,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        stone_button = findViewById<Button>(R.id.stone_button)
-        donkey_button = findViewById<Button>(R.id.donkey_button)
-        stone_button?.setOnClickListener(this)
-        donkey_button?.setOnClickListener(this)
+        val forecast = Forecast(maxTemp = 25f, mineTemp = 10f, humidity = 35f,description = "Día soleado", icon = R.drawable.ico_01)
 
-        //otra forma más kotlinera de hacer
-//        findViewById<Button>(R.id.stone_button).setOnClickListener(this)
-//        findViewById<Button>(R.id.donkey_button).setOnClickListener(this)
-        offlineWeatherImage = findViewById(R.id.offline_weather_image)
-
-        Log.v(TAG, "onCreate")
-        if (savedInstanceState != null){
-            //Log.v(TAG, savedInstanceState.getString("clave"))
-            Log.v(TAG, "savedInstanceState !=null y la clave es: ${savedInstanceState.getString("clave")}")
-        }else{
-            Log.v(TAG, "savedInstanceState == null")
-        }
-
+        setForecast(forecast)
     }
 
-    override fun onClick(p0: View?) {
-        Log.v(TAG,"önClick")
-//        if (p0 == stone_button){
-//            Log.v(TAG,"onClick -> stone_button")
-//        }else{
-//            Log.v(TAG,"onClick -> donkey_button")
-//        }
+    private fun setForecast(forecast: Forecast) {
+        //accedemos a las vistas de la interfaz
+        val forecastImage = findViewById<ImageView>(R.id.forecast_image)
+        val maxTemp = findViewById<TextView>(R.id.max_temp)
+        val minTemp = findViewById<TextView>(R.id.min_temp)
+        val humidity = findViewById<TextView>(R.id.humidity)
+        val forecastDescription = findViewById<TextView>(R.id.forecast_description)
 
-        if (p0 != null){
-            if (p0.id == R.id.stone_button){
-                Log.v(TAG,"onClick -> stone_button")
-            }else if (p0.id == R.id.donkey_button){
-                Log.v(TAG,"onClick -> donkey_button")
-            }
-        }
-
-        when (p0?.id){
-            R.id.stone_button -> {
-                Log.v(TAG,"onClick -> stone_button")
-            }
-            R.id.donkey_button -> {
-                Log.v(TAG,"onClick -> donkey_button")
-            }
-            else -> Log.v(TAG,"onClick -> no se que pulsaron")
-        }
-
-        //forma mas kotlinera
-        //ya no se necesitan las var button
-        Log.v(TAG, when (p0?.id){
-            R.id.stone_button -> "onClick -> stone_button"
-            R.id.donkey_button -> "onClick -> donkey_button"
-            else -> "onClick -> no se que pulsaron"
-        })
-
-        //CAMBIAR IMAGEN
-//        when (p0?.id){
-//            R.id.stone_button -> offlineWeatherImage?.setImageResource(R.drawable.offline_weather)
-//            R.id.donkey_button -> offlineWeatherImage?.setImageResource(R.drawable.offline_weather2)
-//        }
-
-        //forma mas kotlinera
-        offlineWeatherImage?.setImageResource(when (p0?.id){
-            R.id.stone_button -> R.drawable.offline_weather
-            else -> R.drawable.offline_weather2
-        })
+        //actualizamos vista con modelo
+        forecastImage.setImageResource(forecast.icon)
+        forecastDescription.text = forecast.description
+        val maxTempString = getString(R.string.max_temp_format, forecast.maxTemp)
+        val minTempString = getString(R.string.min_temp_format, forecast.mineTemp)
+        val humidityString = getString(R.string.humidity_format, forecast.humidity)
+        maxTemp.text = maxTempString
+        minTemp.text = minTempString
+        humidity.text = humidityString
 
     }
-
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-        Log.v(TAG, "onSaveInstanceState")
-//        if (outState != null){
-//            outState.putString("clave","valor")
-//        }
-        outState?.putString("clave", "valor")
-   }
 }
+
+
+
